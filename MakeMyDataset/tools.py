53b0a3410616ecfd482f5fe0be9d6df9df446c60
SeenivasanSeeni <seeni0424@gmail.com>
2018-06-19 20:26:19 +0530

    Add reset conf.py

--- MakeMyDataset/tools.py
+++ MakeMyDataset/tools.py
@@ -9,3 +9,8 @@ def writeConf(path,conf):
     with open(path,"w") as file:
         json.dump(obj=conf,fp=file)
     return
+
+def resetConf(path):
+    conf={"seeni":1,"NOTSeeni":1}
+    writeConf(path,conf)
+    return
